a144257c1d29d6a3b4e7359fea37e9fb81ca0d5b
Patrick Perini <pcperini@gmail.com>
2015-08-03 12:25:36 -0700

    adds signature test

--- JavascriptEngineTests/WKWebView+JSEngineInjectionTests.swift
+++ JavascriptEngineTests/WKWebView+JSEngineInjectionTests.swift
@@ -38,4 +38,36 @@ class WKWebView_JSEngineInjectionTests: XCTestCase {
         
         self.waitForExpectationsWithTimeout(WKWebView_JSEngineInjectionTests.defaultTimeout, handler: nil)
     }
+    
+    func testSignature() {
+        let expectation = self.expectationWithDescription("signature-generated call worked")
+        let expectedValue = "goodbye world"
+        
+        let webView = WKWebView()
+        webView.loadHTMLString("<html>" +
+            "<head>" +
+                "<source type='text/javascript'>" +
+                    "var testGlobalVar = 'hello world';" +
+                "</source>" +
+            "</head>" +
+        "</html>", baseURL: nil)
+        
+        let engine = JSEngine(sourceString: "function testFunc() {" +
+            "testGlobalVar = '\(expectedValue)';" +
+        "}")
+        
+        webView.injectEngine(engine)
+        engine.load {
+            let call = engine.signatureForFunction("testFunc")
+            webView.evaluateJavaScript(call!) { (_: (AnyObject!, NSError!)) in
+                webView.evaluateJavaScript("testGlobalVar") { (result: (AnyObject!, NSError!)) in
+                    XCTAssertNotNil(result.0 as? String, "Result is not a string")
+                    XCTAssertEqual(expectedValue, result.0 as! String, "Results does not equal expected value")
+                    expectation.fulfill()
+                }
+            }
+        }
+        
+        self.waitForExpectationsWithTimeout(10, handler: nil)
+    }
 }
